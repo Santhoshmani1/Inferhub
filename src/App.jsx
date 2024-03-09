5d3de48cc81828512d47a8c846d2e5856564219d
Santhosh <pitakasanthosh@gmail.com>
2024-03-09 16:09:46 +0530

    Add TextToImage page route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import TextToImage from "./pages/TextToImage";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+          <Route path="/TTI" element={<TextToImage />} />
           <Route path="*" element={<NotFound />} />
       </Routes>
     </>

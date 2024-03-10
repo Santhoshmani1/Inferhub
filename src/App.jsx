b8c168fb3c64882ba727989e9d57e17e024ffce3
Santhosh <pitakasanthosh@gmail.com>
2024-03-10 21:55:40 +0530

    Add Routes to Notfound and TextToImage paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import TextToImage from "./pages/TextToImage";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/TTI" element={<TextToImage />} />
+          <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );

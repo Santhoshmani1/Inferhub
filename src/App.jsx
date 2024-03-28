c27a568158321991ef25c5c89ad3ecb86e13baa0
santhosh <pitakasanthosh@gmail.com>
2024-03-28 16:29:17 +0530

    Feat: Add Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import TextToImage from "./pages/TextToImage";
 import NotFound from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
+
   return (
     <>
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/TTI" element={<TextToImage />} />
-          <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
